851214f6e6efaebfe126d04834a51d70300cf88d
h-banii <121690516+h-banii@users.noreply.github.com>
2025-08-17 04:11:45 -0300

    pkgs/clip-studio-paint: Add buildClipStudioPaint function

--- pkgs/clip-studio-paint/default.nix
+++ pkgs/clip-studio-paint/default.nix
@@ -1,22 +1,29 @@
 { callPackage, fetchurl, ... }:
 let
   builders = callPackage ./builders.nix { };
+
+  buildClipStudioPaint =
+    { version, installerHash }:
+    let
+      ver = builtins.replaceStrings [ "." ] [ "" ] version;
+    in
+    callPackage ./base.nix {
+      inherit (builders) buildWineApplication buildInstallShield;
+      inherit version;
+      installer = fetchurl {
+        url = "https://vd.clipstudio.net/clipcontent/paint/app/${ver}/CSP_${ver}w_setup.exe";
+        hash = installerHash;
+      };
+    };
 in
 {
-  clip-studio-paint-v1 = callPackage ./base.nix {
-    inherit (builders) buildWineApplication buildInstallShield;
+  clip-studio-paint-v1 = buildClipStudioPaint {
     version = "1.13.2";
-    installer = fetchurl {
-      url = "https://vd.clipstudio.net/clipcontent/paint/app/1132/CSP_1132w_setup.exe";
-      hash = "sha256-cFJcShjYMxwUKo7OJVRxQE3R/nrKa8cuqZWjA9Gmq/g=";
-    };
+    installerHash = "sha256-cFJcShjYMxwUKo7OJVRxQE3R/nrKa8cuqZWjA9Gmq/g=";
   };
-  clip-studio-paint-v2 = callPackage ./base.nix {
-    inherit (builders) buildWineApplication buildInstallShield;
+
+  clip-studio-paint-v2 = buildClipStudioPaint {
     version = "2.0.6";
-    installer = fetchurl {
-      url = "https://vd.clipstudio.net/clipcontent/paint/app/206/CSP_206w_setup.exe";
-      hash = "sha256-7aljWvkwjqOKIofUk202Cw4iIq6qxBwYB8Q8K2gqPEw=";
-    };
+    installerHash = "sha256-7aljWvkwjqOKIofUk202Cw4iIq6qxBwYB8Q8K2gqPEw=";
   };
 }
