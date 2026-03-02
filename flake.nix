7b8326fe02d7d4dad5681c2d4a4a35225226607a
h-banii <121690516+h-banii@users.noreply.github.com>
2026-03-02 20:08:55 -0300

    flake: nixpkgs-stable: 25.05 -> 25.11

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/25.05";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/25.11";
   };
 
   outputs =
